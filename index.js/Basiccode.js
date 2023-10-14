d9df8e76f6bae439be58df405bfbf1b658fa8c9e
Harsh Malviya <137219262+coderharshm@users.noreply.github.com>
2023-10-14 17:46:47 +0000

    container config done

---
+++ index.js/Basiccode.js
@@ -0,0 +1,8 @@
+const accountId= 485464 //hum isme change nhi kr sakte hai ek bar jo element de diya wo fix hai
+let accountEmai="harshmalviya009@gmail.com" 
+var accountpassword="12345"
+accountCity="jaipur"
+
+accountId=2
+
+console.log(accountId);
