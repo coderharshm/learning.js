f9fd19a56e4f586a42da8793dd9b8eb8ae9d28b7
Harsh Malviya <137219262+coderharshm@users.noreply.github.com>
2023-10-16 17:02:08 +0000

    03_conversionandoperation

---
+++ index/03_conversionandoperation.js
@@ -0,0 +1,35 @@
+let score="harsh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//jab number ko conver hai to wo easliy convert ho jata hai
+// ex-"33"=>33
+//"33abc"=>NaN(Not an Number) And Type NaN is number
+//true=>1 and false=>0
+
+let isloggedIn
+
+let booleanIsLoggedIn=Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//""=>false
+//"hitesh"=>true
+
+let someNumber=33
+
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+/*
+hum ne isme int value ko string me convert kr k dekha
+*/
+
+
