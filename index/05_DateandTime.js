3bed456efce6e1e0fabf8b4ad82a4f17362a6454
Harsh Malviya <137219262+coderharshm@users.noreply.github.com>
2023-10-17 17:42:19 +0000

    datatype_summary.js

---
+++ index/05_DateandTime.js
@@ -0,0 +1,23 @@
+//Date
+
+let myDate= new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+
+//let myCreatedDate=new Date(2023, 1, 23)
+
+//console.log(myCreatedDate.toDateString());
+
+let myCreateDate = new Date("01-14-2023")
+
+//console.log(myCreateDate);
+
+let myTimeStamp= Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());
+console.log(myCreateDate.getTime());
+
+
+
