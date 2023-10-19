174f147c9273e14c6c46a296454025856b27db62
Harsh Malviya <137219262+coderharshm@users.noreply.github.com>
2023-10-19 17:15:47 +0000

    06_Array_part_1.js

---
+++ index/06_Array_part_1.js
@@ -0,0 +1,53 @@
+const myArr=[0,1,2,3,4,5]
+
+//console.log(myArr[5]);
+
+const myheros= ["spider-man","iron-man","harryporter"];
+
+const myArr2= new Array(1,2,3,4)
+//console.log(myArr[1]);
+
+
+//Array Methods
+//myArr.push(6) yaha pe humne array me elemrnt add kr diya hai
+//myArr.push(7) ============|same|============
+//myArr.pop()   yaha pe jo subse last element add kr rha hai wo delete ho jye ga. 
+
+//myArr.unshift(12) yaha pe hum values ko ek-ek kr k shift krte jate hai starting me humne jo value add ho jyi gi.
+
+//myArr.shift()  me starting ki value remove ho jyi gi..
+
+ //console.log(myArr.includes(9));// isme chechk krte hai ki array me ye value hai ki nhi..
+ //console.log(myArr.indexOf(3));// yaha hum array vme value k index check krte hai
+
+ //console.log(myArr);
+
+ //slice, splice
+
+
+/* console.log("A", myArr);
+
+ const myn1=myArr.slice(1,3)
+
+ console.log(myn1);
+ console.log("B ", myArr);
+
+ const myn2 = myArr.splice(1,3)
+ console.log(myn2);
+*/
+//const cars = new Array("saab", "Volvo","BMW");
+
+/*const fruits=['Banana',"Orange","Apple","mango"]
+
+ let length= fruits.length;
+
+ console.log(length);
+*/
+
+
+const fruits=["Banana","Orange","Apple"]
+
+fruits[fruits.length]="Lemon";
+
+console.log(fruits)
+
